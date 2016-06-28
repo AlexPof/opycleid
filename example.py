88bc788037b3e5114a5ee8bb8692ed2952333f94
Alexandre Popoff <al.popoff@free.fr>
2016-06-28 22:01:21 +0200

    Add files via upload

--- example.py
+++ example.py
@@ -10,5 +10,5 @@ my_group = PRL_Group()
 #Create a path K-Net for the famous Beethoven example
 my_knet = KNet(my_group)
 my_knet.add_vertices(["C","a","F","d","Bb","g","Eb","c","Gs","f","Cs","bb"])
-my_knet.pathKNet_from_vertices()
-my_knet.print_KNet()
+my_knet.path_knet_from_vertices()
+my_knet.print_knet()
