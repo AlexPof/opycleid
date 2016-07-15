56b3d3fa4e9d3aedf42efee0c9d8eae5d0c34c51
Alexandre Popoff <al.popoff@free.fr>
2016-07-15 08:51:49 +0200

    Update example.py

--- example.py
+++ example.py
@@ -2,8 +2,8 @@
 ###### Copyright (c) 2016, Alexandre Popoff 
 ###
 
-from opycleid.Monoids import PRL_Group
-from opycleid.KNetAnalysis import KNet
+from opycleid.monoids import PRL_Group
+from opycleid.knetanalysis import KNet
 
 my_group = PRL_Group()
 
