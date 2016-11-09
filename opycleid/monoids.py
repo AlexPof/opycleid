d93ed9f07bd2fb93c232c61477f9650ff1e0d87a
Alexandre Popoff <al.popoff@free.fr>
2016-11-09 12:04:44 +0100

    Added UPL, S, T, ST Monoids
    Added the UPL, S, T, and ST monoids for relational analysis on major, minor, and augmented triads

--- opycleid/monoids.py
+++ opycleid/monoids.py
@@ -432,3 +432,167 @@ class Right_Z3Q8_Group(MonoidAction):
                         x = x.dot(T)
                 self.operations["T^"+str(i)] = x
                 self.operations["J^"+str(i)] = x.dot(J)
+
+################################################
+###### UPL MONOID
+###
+
+class UPL_Monoid(MonoidAction):
+    def __init__(self):
+        self.objects = {"C":0,"Cs":1,"D":2,"Eb":3,"E":4,"F":5,"Fs":6,"G":7,"Gs":8,"A":9,"Bb":10,"B":11,
+                        "c":12,"cs":13,"d":14,"eb":15,"e":16,"f":17,"fs":18,"g":19,"gs":20,"a":21,"bb":22,"b":23,
+                        "C_aug":24,"F_aug":25,"D_aug":26,"G_aug":27}
+        
+        P = np.zeros((28,28),dtype=bool)
+        for i in range(12):
+            P[i+12,i]=True
+            P[i,i+12]=True
+        for i in range(24,28):
+            P[i,i]=True
+
+        L = np.zeros((28,28),dtype=bool)
+        for i in range(12):
+            L[(i+4)%12+12,i]=True
+            L[(i+8)%12,12+i]=True
+        for i in range(24,28):
+            L[i,i]=True
+
+        U = np.zeros((28,28),dtype=bool)
+
+        for i in range(12):
+            U[24+(i%4),i]=True
+            U[i,24+(i%4)]=True
+
+            U[24+((i+3)%4),12+i]=True
+            U[12+i,24+((i+3)%4)]=True
+
+        self.generators = {"P":P,"L":L,"U":U}
+        self.generate_monoid()
+
+################################################
+###### 	S-MONOID
+###
+
+class S_Monoid(MonoidAction):
+    def __init__(self):
+        self.objects = {"C":0,"Cs":1,"D":2,"Eb":3,"E":4,"F":5,"Fs":6,"G":7,"Gs":8,"A":9,"Bb":10,"B":11,
+                        "c":12,"cs":13,"d":14,"eb":15,"e":16,"f":17,"fs":18,"g":19,"gs":20,"a":21,"bb":22,"b":23,
+                        "C_aug":24,"F_aug":25,"D_aug":26,"G_aug":27}
+        
+        S = np.zeros((28,28),dtype=bool)
+
+        for i in range(12):
+            S[24+(i%4),i]=True
+            S[i,24+(i%4)]=True
+
+            S[24+((i+3)%4),12+i]=True
+            S[12+i,24+((i+3)%4)]=True
+            
+            S[12+i,i]=True
+            S[i,12+i]=True
+            
+            S[12+(i+4)%12,i]=True
+            S[i,12+(i+4)%12]=True
+
+        self.generators = {"S":S}
+        self.generate_monoid()
+
+
+################################################
+###### 	T-MONOID
+###
+
+class T_Monoid(MonoidAction):
+    def __init__(self):
+        self.objects = {"C":0,"Cs":1,"D":2,"Eb":3,"E":4,"F":5,"Fs":6,"G":7,"Gs":8,"A":9,"Bb":10,"B":11,
+                        "c":12,"cs":13,"d":14,"eb":15,"e":16,"f":17,"fs":18,"g":19,"gs":20,"a":21,"bb":22,"b":23,
+                        "C_aug":24,"F_aug":25,"D_aug":26,"G_aug":27}
+        
+        T = np.zeros((28,28),dtype=bool)
+
+        for i in range(12):
+            T[(i+4)%12,i]=True
+            T[i,(i+4)%12]=True
+            
+            T[(i+8)%12,i]=True
+            T[i,(i+8)%12]=True
+            
+            T[12+(i+1)%12,i]=True
+            T[i,12+(i+1)%12]=True
+
+            T[12+(i+5)%12,i]=True
+            T[i,12+(i+5)%12]=True
+
+            T[24+((i+3)%4),i]=True
+            T[i,24+((i+3)%4)]=True
+               
+        for i in range(12):
+            T[24+(i%4),12+i]=True
+            T[12+i,24+(i%4)]=True
+
+            T[12+(i+4)%12,12+i]=True
+            T[12+i,12+(i+4)%12]=True
+            
+            T[12+(i+8)%12,12+i]=True
+            T[12+i,12+(i+8)%12]=True
+
+        self.generators = {"T":T}
+        self.generate_monoid()
+
+
+################################################
+###### 	ST-MONOID
+###
+
+class ST_Monoid(MonoidAction):
+    def __init__(self):
+        self.objects = {"C":0,"Cs":1,"D":2,"Eb":3,"E":4,"F":5,"Fs":6,"G":7,"Gs":8,"A":9,"Bb":10,"B":11,
+                        "c":12,"cs":13,"d":14,"eb":15,"e":16,"f":17,"fs":18,"g":19,"gs":20,"a":21,"bb":22,"b":23,
+                        "C_aug":24,"F_aug":25,"D_aug":26,"G_aug":27}
+        
+
+        S = np.zeros((28,28),dtype=bool)
+
+        for i in range(12):
+            S[24+(i%4),i]=True
+            S[i,24+(i%4)]=True
+
+            S[24+((i+3)%4),12+i]=True
+            S[12+i,24+((i+3)%4)]=True
+            
+            S[12+i,i]=True
+            S[i,12+i]=True
+            
+            S[12+(i+4)%12,i]=True
+            S[i,12+(i+4)%12]=True
+
+        T = np.zeros((28,28),dtype=bool)
+
+        for i in range(12):
+            T[(i+4)%12,i]=True
+            T[i,(i+4)%12]=True
+            
+            T[(i+8)%12,i]=True
+            T[i,(i+8)%12]=True
+            
+            T[12+(i+1)%12,i]=True
+            T[i,12+(i+1)%12]=True
+
+            T[12+(i+5)%12,i]=True
+            T[i,12+(i+5)%12]=True
+
+            T[24+((i+3)%4),i]=True
+            T[i,24+((i+3)%4)]=True
+               
+        for i in range(12):
+            T[24+(i%4),12+i]=True
+            T[12+i,24+(i%4)]=True
+
+            T[12+(i+4)%12,12+i]=True
+            T[12+i,12+(i+4)%12]=True
+            
+            T[12+(i+8)%12,12+i]=True
+            T[12+i,12+(i+8)%12]=True
+
+        self.generators = {"S":S,"T":T}
+        self.generate_monoid()
