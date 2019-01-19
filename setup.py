0f8454374e0b31744cf3ffca3cd5afaeadcf5c72
Alexandre Popoff <al.popoff@free.fr>
2019-01-19 19:35:27 +0100

    ADD: version change

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ except ImportError:
     sys.exit(1)
 
 setup(name='opycleid',
-      version='0.2.6',
+      version='0.3.0',
       description='Transformational music analysis in Python',
       author='Alexandre Popoff',
       author_email='al.popoff@free.fr',
